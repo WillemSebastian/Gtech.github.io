ca4bdd9724d3cff0c7914cdf4f4a2a62860d56fb
willem Sebastian <willem1991@yahoo.co.id>
2020-03-12 00:08:03 +0700

    change to UpdatedState

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -7,16 +7,17 @@ const initialState = {
 export default function(state = initialState, action) {
   switch (action.type) {
     case ADD_TO_CART: {
-      var state = { ...state };
-      if (state.products.includes(action.payload.data)) {
-        state.products.count = state.products.count + action.payload.count;
+      var updatedState = { ...state };
+      if (updatedState.products.includes(action.payload.data)) {
+        updatedState.products.count =
+          state.products.count + action.payload.count;
       } else {
         action.payload.data["count"] = action.payload.count;
-        state.products.push(action.payload.data);
+        updatedState.products.push(action.payload.data);
       }
       return {
         ...state,
-        state
+        updatedState
       };
     }
     default:
