d02676485a866a2acdc1732ba6a1148aee4ccc7c
willem Sebastian <willem1991@yahoo.co.id>
2020-03-12 00:10:55 +0700

    Updates

---
+++ precache-manifest.69426b2eb2802d7adf79b16952f914dd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d50611495eaf87092162d2a07ee23b",
+    "url": "/Gtech.github.io/index.html"
+  },
+  {
+    "revision": "fbdaaa5b89fa89e4ed42",
+    "url": "/Gtech.github.io/static/css/main.efeb747f.chunk.css"
+  },
+  {
+    "revision": "bfb5fb9b16f99dd75864",
+    "url": "/Gtech.github.io/static/js/2.ca75580e.chunk.js"
+  },
+  {
+    "revision": "fbdaaa5b89fa89e4ed42",
+    "url": "/Gtech.github.io/static/js/main.f26fb522.chunk.js"
+  },
+  {
+    "revision": "bb45150cba92a1129ad1",
+    "url": "/Gtech.github.io/static/js/runtime~main.3b44b98c.js"
+  },
+  {
+    "revision": "e643a6cf77b4635dda856f10fa9b7db4",
+    "url": "/Gtech.github.io/static/media/bg11.e643a6cf.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/Gtech.github.io/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/Gtech.github.io/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/Gtech.github.io/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/Gtech.github.io/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
